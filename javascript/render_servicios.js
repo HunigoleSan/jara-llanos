6f22472ff1397fb148350ad17b53551a133919f5
HunigoleSan <soy.hugo.pe@gmail.com>
2025-03-27 15:29:11 -0500

    Fix, style, js: nueva estructura para las card de servicios, se manipulo el componente js, la data y el estilo css, con la finalidad de mostrar mas información en las card al pasarle cursor

--- javascript/render_servicios.js
+++ javascript/render_servicios.js
@@ -4,7 +4,7 @@ import Servicios_cards from "./data/servicios_cards.js";
 window.document.addEventListener("DOMContentLoaded", function(){
     let contenedor_servicios = document.getElementById("servicios-cards")
     Servicios_cards.forEach(iterador => {
-        let component =  cards(iterador.icono, iterador.nombre, iterador.descripcion)
+        let component =  cards(iterador.icono, iterador.nombre, iterador.descripcion, iterador.tipos)/* undefined */
         contenedor_servicios.insertAdjacentHTML("beforeend", component)
     });
 })
