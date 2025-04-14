96639fcdc669552156214f56f91a6fcdccc774cb
HunigoleSan <soy.hugo.pe@gmail.com>
2025-04-14 11:47:04 -0500

    Animaciones scroll adaptados

--- javascript/animations/employeds_card.js
+++ javascript/animations/employeds_card.js
@@ -1,11 +1,17 @@
 let bandera = false;
+let widthBandera = false
+let scrollYe = 0
 
 window.document.addEventListener("scroll", ()=>{
-    if(window.scrollY > 2500 && !bandera){
+    
+    modificarAncho()
+
+
+    if(window.scrollY > scrollYe && !bandera){
         efectoAparicion()
         console.log("efecto aparicion, estoy en escritorio")
         bandera = true
-    }else if(window.scrollY < 2500 && bandera){
+    }else if(window.scrollY < scrollYe && bandera){
         console.log("efecto ocultar")
         bandera = false
         efectoOcultar()
@@ -13,6 +19,46 @@ window.document.addEventListener("scroll", ()=>{
     
     
 })
+function modificarAncho(){
+    if(window.innerWidth > 1239 && !widthBandera){
+        scrollYe = 2124
+        widthBandera = true
+    }else if(window.innerWidth < 1240 && bandera){
+        widthBandera = false
+
+    }
+
+    else if(window.innerWidth > 1082 && !widthBandera){
+        scrollYe = 2552
+        widthBandera = true
+    }else if(window.innerWidth < 1083 && bandera){
+        widthBandera = false
+    }
+    
+    else if(window.innerWidth > 817 && !widthBandera){
+        scrollYe = 3505
+        widthBandera = true
+    }else if(window.innerWidth < 818 && bandera){
+        widthBandera = false
+
+    }
+
+    else if(window.innerWidth > 590 && !widthBandera){
+        scrollYe = 3444
+        widthBandera = true
+    }else if(window.innerWidth < 591 && bandera){
+        widthBandera = false
+
+    }
+    
+    else if(window.innerWidth > 280 && !widthBandera){
+        scrollYe = 4677
+        widthBandera = true
+    }else if(window.innerWidth < 281 && bandera){
+        widthBandera = false
+
+    }
+}
 
 
 function efectoAparicion(){
@@ -26,8 +72,8 @@ function efectoAparicion(){
     });
 }
 
-function efectoOcultar(){
-    const cardsEmployeds = document.querySelectorAll('.employeds_card') /* NodeElement */
+/* function efectoOcultar(){
+    const cardsEmployeds = document.querySelectorAll('.employeds_card') 
     
     cardsEmployeds.forEach((element,index) => {
         setTimeout(()=>{
@@ -36,4 +82,4 @@ function efectoOcultar(){
         },300 * (index + 1))
     });
 }
-
+ */
--- javascript/animations/service_card.js
+++ javascript/animations/service_card.js
@@ -1,14 +1,17 @@
 let bandera = false
+let scrollYs = 0
+let widthBandera = false
 
 window.document.addEventListener('scroll',()=>{
-    if(window.scrollY > 330 && !bandera){
+    
+    efectoAparicion()
+    if(window.scrollY > scrollYs && !bandera){
         efectoAparicion()
         console.log("efecto aparicion ")
         bandera = true
-    }else if(window.scrollY < 330 && bandera){
-        console.log("efecto ocultar")
+    }else if(window.scrollY < scrollYs && bandera){
         bandera = false
-        efectoOcultar()
+        
     }
 
     
@@ -18,6 +21,47 @@ window.document.addEventListener('scroll',()=>{
 window.document.addEventListener('DOMContentLoaded',()=>{
     
 })
+
+function modificarAncho(){
+    if(window.innerWidth > 1239 && !widthBandera){
+        scrollYs = 32
+        widthBandera = true
+    }else if(window.innerWidth < 1240 && bandera){
+        widthBandera = false
+
+    }
+
+    else if(window.innerWidth > 1082 && !widthBandera){
+        scrollYs = 330
+        widthBandera = true
+    }else if(window.innerWidth < 1083 && bandera){
+        widthBandera = false
+    }
+    
+    else if(window.innerWidth > 817 && !widthBandera){
+        scrollYs = 229
+        widthBandera = true
+    }else if(window.innerWidth < 818 && bandera){
+        widthBandera = false
+
+    }
+
+    else if(window.innerWidth > 500 && !widthBandera){
+        scrollYs = 160
+        widthBandera = true
+    }else if(window.innerWidth < 501 && bandera){
+        widthBandera = false
+
+    }
+    
+    else if(window.innerWidth > 280 && !widthBandera){
+        scrollYs = 120
+        widthBandera = true
+    }else if(window.innerWidth < 281 && bandera){
+        widthBandera = false
+
+    }
+}
 
 function efectoAparicion(){
     const cardList = document.querySelectorAll('.card') /* NodeElement */
@@ -30,13 +74,3 @@ function efectoAparicion(){
     });
 }
 
-function efectoOcultar(){
-    const cardList = document.querySelectorAll('.card') /* NodeElement */
-    
-    cardList.forEach((element,index) => {
-        setTimeout(()=>{
-            element.classList.remove("efecto-aparicion")
-
-        },300 * (index + 1))
-    });
-}
